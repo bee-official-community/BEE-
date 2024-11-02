eda63570a0bff0ef3dbdd331cef702cf9e80ead2
이승환 <seaung13@gmail.com>
2024-11-02 22:44:28 +0900

    Initial commit

---
+++ src/main/java/com/bee/beehomepagebackend/BeeHomepageBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bee.beehomepagebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeHomepageBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeeHomepageBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bee/beehomepagebackend/BeeHomepageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bee.beehomepagebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeeHomepageBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
